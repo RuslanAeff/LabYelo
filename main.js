cd0c7a00d3742acf597fdb953a0f24105c828a05
Ruslan Aliyev <ruslanra@code.edu.az>
2023-09-09 17:42:19 +0400

    added currency and cashback

--- main.js
+++ main.js
@@ -4,13 +4,19 @@ if(localStorage.getItem("basket") == null)
 }
 
 let btns = document.querySelectorAll(".add-btn");
+
 btns.forEach(btn => {
     btn.addEventListener("click",(e)=>{
         let cartName = e.target.parentElement.parentElement.children[0].children[0].textContent;
+        let cartCurrency= e.target.parentElement.parentElement.children[0].children[2].children[1].children[1].textContent;
+        let cartCashback= e.target.parentElement.parentElement.children[0].children[2].children[2].children[1].textContent;
+
 
             let basketList = JSON.parse(localStorage.getItem("basket"));
              basketList.push({
-                 name: cartName
+                 name: cartName,
+                 currency: cartCurrency,
+                 cashback: cartCashback
              })
             localStorage.setItem("basket",JSON.stringify(basketList))
     })
